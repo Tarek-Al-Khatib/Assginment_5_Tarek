40156c03a59b9ebe97c2006fd1dcc03512a9e392
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-10-26 15:04:57 +0300

    added all event listeners with necessary logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,39 @@
-var start = document.getElementById("start");
-var end = document.getElementById("end");
-var boundaries = document.querySelectorAll(".boundary");
+//chatgpt
+window.onload = () => {
+  //
+  var start = document.getElementById("start");
+  var end = document.getElementById("end");
+  var lost = false;
+  var boundaries = document.querySelectorAll(".boundary");
+
+  var started = false;
+  var ended = true;
+  var statusOfTheGame = document.getElementById("status");
+
+  //https://stackoverflow.com/questions/8318591/javascript-addeventlistener-using-to-create-a-mouseover-effect
+  //https://www.w3schools.com/jsref/event_onmouseover.asp
+  start.addEventListener("mouseover", function () {
+    started = true;
+    ended = false;
+    lost = false;
+    statusOfTheGame.innerHTML = "Started ! Let's win for Taha !!!";
+  });
+
+  end.addEventListener("mouseover", function () {
+    if (!lost) {
+      ended = true;
+      started = false;
+      statusOfTheGame.innerHTML = "You win !";
+    }
+  });
+
+  boundaries.forEach((boundary) => {
+    boundary.addEventListener("mouseover", function () {
+      if (started) {
+        ended = true;
+        lost = true;
+        statusOfTheGame.innerHTML = "You lost !";
+      }
+    });
+  });
+};
